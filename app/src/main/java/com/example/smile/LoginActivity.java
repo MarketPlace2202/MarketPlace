6bdb32c204bd94a6c3c0ebdf467d0307b268c520
sudhakar <sudhakar.bittu@gmail.com>
2022-11-15 14:03:14 +0530

    Adding Singup Activity

--- app/src/main/java/com/example/smile/LoginActivity.java
+++ app/src/main/java/com/example/smile/LoginActivity.java
@@ -2,13 +2,36 @@ package com.example.smile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginButton;
+    private Button createAcctButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton = findViewById(R.id.email_signin);
+        createAcctButton = findViewById(R.id.email_signup);
+        // Listing Create Account button in login Page
+        createAcctButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,CreateAccountActivity.class));
+            }
+        });
+
+        // Listening Singin in login Button smile-92e53
+
     }
 }
